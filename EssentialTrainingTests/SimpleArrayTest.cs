b93432d9ecc27a625540502bbbdd472d42e17a87
Ryan Robinson <274507+Veranith@users.noreply.github.com>
2021-08-28 20:24:01 -0600

    adds simple array class and unit tests

---
+++ EssentialTrainingTests/SimpleArrayTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using EssentialTraining;
+
+namespace EssentialTrainingTests
+{
+    [TestClass]
+    public class SimpleArrayTest
+    {
+        [TestMethod]
+        public void TestInstantiation()
+        {
+            var testInstance = new SimpleArray();
+            Assert.AreEqual(testInstance.groceryList.Length, 4);
+            Assert.AreEqual(testInstance.groceryList[1], "Milk");
+        }
+
+        [TestMethod]
+        public void TestToString()
+        {
+            var testInstance = new SimpleArray();
+            Assert.IsTrue(testInstance.ToString().StartsWith("There are"));
+        }
+    }
+}
